7c67066951a88bdf7a52f966d3805a05e17a05ac
Marie Rose Dela Torre <116274408+Codingmarie@users.noreply.github.com>
2023-01-08 17:13:33 +0800

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,46 +1,21 @@
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-import User from '../models/User.js'
+import jwt from "jsonwebtoken"
 
-export const register = async (req, res) => {
+export const verifyToken = async (req, res, next) => {
     try {
-        const { firstName, lastName, email, password } = req.body
-        const salt = await bcrypt.genSalt()
-        const encryptedPassword = await bcrypt.hash(password, salt)
-        const newUser = await User.create({
-            firstName,
-            lastName,
-            email,
-            password: encryptedPassword
-        })
-        const savesUser = await newUser.save()
-        res.status(201).json(savedUser)
-    } catch (err) {
-        res.status(500).json({ error: err.message})
-    }
-} 
+        let token = req.header("Authorization")
 
-export const login = async (req, res) => {
-    try {
-        const {email, password} = req.body
-
-        const user = await User.findOne({
-            email: email
-        })
-
-        if (!user) return res.status(400).json({msg:'invalid email/password'})
-        
-        const isPasswordValid = await bcrypt.compare(password, user.password)
-
-        if (isPasswordValid) {
-            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
-            user.password = '***'
-            res.status(200).json({toke, user})
-        } else {
-            res.status(400).json({msg: 'invalid credentials'})
+        if (!token) {
+            return res.status(401).send("Unauthorized")
         }
-    } catch (err) {
+
+        if (token.startsWith("Bearer")){
+            token = token.slice(7, token.length).trimleft()
+        }
+
+        const verified = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = verified;    
+        next();                                                                      
+    } catch(err) {
         res.status(500).json({ error: err.message})
     }
 }
-
